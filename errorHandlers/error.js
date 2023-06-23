02cd1eab5a1ed5e77e66d6b98886fb1a3362bc9f
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-23 11:01:35 +0300

    Add error handling for CastError

--- errorHandlers/error.js
+++ errorHandlers/error.js
@@ -1,9 +1,11 @@
-const {Error: {ValidationError}} = require('mongoose');
+const {Error: {ValidationError, CastError}} = require('mongoose');
 
 module.exports.errorHandler = async (err, rea, res, next) => {
     if (err instanceof ValidationError) {
         return res.status(400).send({errors: `Validation failed. Reason: ${err.errors.name}`});
     }
-
+    if (err instanceof CastError && err.kind === 'ObjectId') {
+        return res.status(404).send({errors: `Instance not found. Invalid id.`});
+    }
     res.status(500).send({errors: 'Unknown error'});
 }
