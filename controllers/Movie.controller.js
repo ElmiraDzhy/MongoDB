70dc0ddd220e617d75ae65da69bc78aa32d86258
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-23 09:23:23 +0300

    Add project structure

---
+++ controllers/Movie.controller.js
@@ -0,0 +1,12 @@
+module.exports.createOne = async (req, res, next) => {}
+module.exports.getAll = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
+// module.exports.createOne = async (req, res, next) => {}
---
+++ routes/movieRouter.js
@@ -0,0 +1,8 @@
+const express = require(express)
+const movieRouter = express.Router()
+const MovieController = require('../controllers/Movie.controller')
+
+movieRouter.get('/', MovieController.getAll);
+movieRouter.post('/', MovieController.createOne);
+
+module.exports = movieRouter;
